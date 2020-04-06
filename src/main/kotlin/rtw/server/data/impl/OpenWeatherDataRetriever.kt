d45cc666a455235e92d5b163bccee8c46222939e
r4v3n6101 <raven6107@gmail.com>
2020-04-06 08:01:10 +0300

    Code cleanup

--- src/main/kotlin/rtw/server/data/impl/OpenWeatherDataRetriever.kt
+++ src/main/kotlin/rtw/server/data/impl/OpenWeatherDataRetriever.kt
@@ -7,24 +7,25 @@ import java.net.URL
 import java.time.Duration
 import java.time.ZoneOffset
 import java.util.concurrent.Executors
-import java.util.concurrent.Future
-import java.util.concurrent.FutureTask
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicReference
 
 class OpenWeatherDataRetriever(
-        private val updateDelay: Long = Duration.ofMinutes(10).toMillis(),
         private val offset: ZoneOffset = ZoneOffset.of("+3"),
+        updateDelay: Long = Duration.ofMinutes(10).toMillis(),
         apiKey: String = "0b26f2a80235ec3a9986ff95d551e24f",
         city: String = "moscow,ru"
 ) : DataRetriever {
 
     private val weatherUrl = URL("https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric")
-    private var futureRtwData: Future<RTWData?> = FutureTask { null }
-    private var lastConfigUpdate = 0L
+    private var rtwData = AtomicReference(DefaultDataRetriever.retrieve())
+    private val threadPool = Executors.newScheduledThreadPool(5)
 
-    /**
-     * @return null if error ocured, rtw data otherwise
-     */
-    private fun requestData() = try {
+    init {
+        threadPool.scheduleWithFixedDelay(::updateData, 0, updateDelay, TimeUnit.MILLISECONDS)
+    }
+
+    private fun updateData() {
         val response = weatherUrl.readText()
         println("Get response: $response")
         val root = JsonParser().parse(response).asJsonObject
@@ -33,24 +34,15 @@ class OpenWeatherDataRetriever(
         val main = root.getAsJsonObject("main")
         val clouds = root.getAsJsonObject("clouds")
 
-        RTWData(
+        rtwData.set(RTWData(
                 latitude = coord["lat"].asFloat,
                 longitude = coord["lon"].asFloat,
                 zoneOffset = offset,
                 temperature = main["temp"].asFloat,
                 visibility = root["visibility"].asInt.toFloat(),
                 cloudiness = clouds["all"].asInt.toFloat()
-        )
-    } catch (t: Throwable) {
-        t.printStackTrace()
-        null
+        ))
     }
 
-    override fun retrieve(): RTWData {
-        if (lastConfigUpdate + updateDelay < System.currentTimeMillis()) {
-            futureRtwData = Executors.newCachedThreadPool().submit(::requestData)
-            lastConfigUpdate = System.currentTimeMillis()
-        }
-        return futureRtwData.get() ?: DefaultDataRetriever.retrieve()
-    }
+    override fun retrieve(): RTWData = rtwData.get()
 }
--- src/main/kotlin/rtw/server/data/impl/ScriptDataRetriever.kt
+++ src/main/kotlin/rtw/server/data/impl/ScriptDataRetriever.kt
@@ -6,25 +6,19 @@ import java.nio.file.Path
 import javax.script.Invocable
 import javax.script.ScriptEngineManager
 
-class ScriptDataRetriever(scriptDir: Path) : DataRetriever {
+class ScriptDataRetriever(scriptPath: Path) : DataRetriever {
 
     private var _retriever: DataRetriever = DefaultDataRetriever
 
     init {
-        Files.walk(scriptDir).filter {
-            Files.isRegularFile(it) &&
-                    it.toFile().nameWithoutExtension == "script"
-        }.forEach { scriptFile ->
-            println("Found: $scriptFile")
-            val extension = scriptFile.toFile().extension
-            val scriptEngine = ScriptEngineManager().getEngineByExtension(extension)
-            if (scriptEngine is Invocable) {
-                scriptEngine.eval(Files.newBufferedReader(scriptFile))
-                _retriever = scriptEngine.getInterface(DataRetriever::class.java)
-                println("Appropriate script, extension=.$extension")
-            } else {
-                println("ScriptEngine isn't invocable, skip")
-            }
+        val extension = scriptPath.toFile().extension
+        val scriptEngine = ScriptEngineManager().getEngineByExtension(extension)
+        if (scriptEngine is Invocable) {
+            Files.newBufferedReader(scriptPath).use(scriptEngine::eval)
+            _retriever = scriptEngine.getInterface(DataRetriever::class.java)
+            println("Appropriate script, extension=.$extension")
+        } else {
+            println("ScriptEngine isn't invocable, skip")
         }
     }
 
